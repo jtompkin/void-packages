5d74eb97400d73b90d0a9ce007d56e746088bbb7
jtompkin <jtompkin-dev@pm.me>
2026-01-21 11:12:40 -0500

    add a bunch of stuff for dwm,st,iosevka

--- srcpkgs/st/files/config.def.h
+++ srcpkgs/st/files/config.def.h
@@ -107,6 +107,7 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+<<<<<<< HEAD
         /* 8 normal colors */
         "#2e3436",
         "#a40000",
@@ -134,6 +135,35 @@ static const char *colorname[] = {
         "#555555",
         "#e4e4ef", /* default foreground colour */
         "#181818", /* default background colour */
+=======
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
+>>>>>>> 40c5043c (add a bunch of stuff for dwm,st,iosevka)
 };
 
 
